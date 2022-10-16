79cedc5b7425348e7023f6f2c5a05aa21f8e5ade
Lyon <hleon0507@gmail.com>
2022-10-16 17:35:14 -0500

    Ajustando images al grid

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import './Home.css'
-import sombra from '../../assets/img/sombra.jpeg'
-//import umbrella from '../../assets/img/umbrella.jpeg'
+//import sombra from '../../assets/img/sombra.jpeg'
+import pufIco from '../../assets/img/pufIco.png'
+import puffPlaya from '../../assets/img/puffPlaya.jpg'
 import Umbrella_ico from '../../assets/img/Umbrella_ico.png'
 //import Info from '../Box/Info';
+
+const sombra = new URL('../../assets/img/sombra.jpeg', import.meta.url)
+
 
 function Home() {
   return (
@@ -21,10 +25,21 @@ function Home() {
         <img src={Umbrella_ico} alt=""/>
         <h2>WELCOME</h2>
       </div>
-      <div className="grid-item">Pufi PUFF</div>
+
+      <div className="grid-item">
+        Pufi PUFF
+        <img src={pufIco} alt="" />
+        <h2>Description of product. This text is simulation</h2>
+        </div>
+      <div className="grid-item">
+      <img src={puffPlaya} alt=""/>
+        
+      </div>
       <div className="grid-item"></div>
-      <div className="grid-item"></div>
-      <div className="grid-item">Pufi CART</div>
+      <div className="grid-item">
+        Pufi CART
+        
+      </div>
       <div className="grid-item">Pufi NAP</div>
       <div className="grid-item"></div>
     </div>
