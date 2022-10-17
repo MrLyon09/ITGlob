1fa11e96204913a67f161eafba0745755f62f647
Lyon <hleon0507@gmail.com>
2022-10-17 07:39:16 -0500

    styles nav

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import Navbar from './components/navbar/Navbar'
 import Header from './components/header/Header'
 import Home from './components/Home/Home'
 import Network from './components/social/Network'
 
 
 
+
 function App() {
   return (
     <div>
+      <Navbar />
       <Header />
+      
       <Home />
       <Network />
       
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,34 +1,44 @@
 import "./Navbar.css";
-import logo from '../../assets/img/logo.png'
+import logo from "../../assets/img/logo.png";
 
 function Navbar() {
   return (
-    <nav className="navbar container">
+    <nav className="navbar space-between">
       <div className="logo">
         <img src={logo.png} alt="logo" className="ImgLogo" />
       </div>
 
       <div className="section">
+
         <div className="item">
           <span>PUFI PUF</span>
         </div>
+        
         <div className="item">
           <span> PUFI RAIN </span>
         </div>
+        
         <div className="item">
           <span> PUFI CART </span>
         </div>
-        <div className="item">
+        
+        <div className="itemF">
           <span> PUFI NAP </span>
         </div>
+
       </div>
 
       <div className="shopping">
+
         <span>
           Mi cuenta <select> </select>{" "}
         </span>
-        <span>Mi carrito</span>
+        <span>
+          Mi carrito
+        </span>
+      
       </div>
+    
     </nav>
   );
 }
