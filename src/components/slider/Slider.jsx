a884b7b168a402777fbece2334114aca9f4ee8b1
Lyon <hleon0507@gmail.com>
2022-10-17 09:08:03 -0500

    continuando implementacion de bootstrap

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -13,19 +13,22 @@ function Slider() {
             alt="First slide"
             />
             <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h1 className='text-slider'>ESTAR COMODO, <br></br>NUNCA FUE TAN FACIL</h1>
+            <div className="btn-slider">
+             <button>SHOP</button>
+            </div>
+
             </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={500}>
             <img
             className="d-block w-100"
-            src="https://images.pexels.com/photos/5913193/pexels-photo-5913193.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            src="https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             alt="Second slide"
             />
             <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <h1 className='text-slider'>ESTAR COMODO, <br></br>NUNCA FUE TAN FACIL</h1>
+            
             </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -35,10 +38,8 @@ function Slider() {
             alt="Third slide"
             />
             <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
+            <h1 className='text-slider'>ESTAR COMODO, <br></br>NUNCA FUE TAN FACIL</h1>
+            
             </Carousel.Caption>
         </Carousel.Item>
         </Carousel>
